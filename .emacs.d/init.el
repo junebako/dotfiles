d8d4ab499c19c266cb3ce09db7e23350199f751d
june29 <june29.jp@gmail.com>
2013-11-11 21:17:55 +0900

    Add config for golang

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -203,9 +203,21 @@
    (cons '("\\.md" . markdown-mode) auto-mode-alist))
 
 ;; Jade
-
 (require 'jade-mode)
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+;; Go
+(require 'go-mode-load)
+(add-hook 'go-mode-hook
+          '(lambda()
+            (setq c-basic-offset 4)
+            (setq indent-tabs-mode t)
+            (local-set-key (kbd "M-.") 'godef-jump)
+            (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+            (local-set-key (kbd "C-c i") 'go-goto-imports)
+            (local-set-key (kbd "C-c d") 'godoc)))
+
+(add-hook 'before-save-hook 'gofmt-before-save)
 
 ;; Htmlize
 (require 'htmlize)
