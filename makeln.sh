b51b24cb273c0711e86f0616477af275ad11fc4d
june29 <june29.jp@gmail.com>
2019-02-01 00:25:52 +0900

    :wrench: Git config: Use ~/.config/git/ignore

--- makeln.sh
+++ makeln.sh
@@ -5,11 +5,11 @@ DOTFILES=~/.dotfiles
 cd ~/
 
 ln -s $DOTFILES/.atom
+ln -s $DOTFILES/.config
 ln -s $DOTFILES/.docker
 ln -s $DOTFILES/.emacs.d
 ln -s $DOTFILES/.gemrc
 ln -s $DOTFILES/.gitconfig
-ln -s $DOTFILES/.gitignore_global
 ln -s $DOTFILES/.peco
 ln -s $DOTFILES/.rspec
 ln -s $DOTFILES/.screenrc
@@ -19,7 +19,3 @@ ln -s $DOTFILES/.zprofile
 ln -s $DOTFILES/.zsh.d
 ln -s $DOTFILES/.zshenv
 ln -s $DOTFILES/.zshrc
-
-cd ~/.config
-
-ln -s $DOTFILES/.config/karabiner
