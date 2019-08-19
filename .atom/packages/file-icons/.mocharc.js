e52165c707d8b9b19245eca81a081d86198e91f6
june29 <june29.jp@gmail.com>
2019-08-19 22:11:16 +0900

    :up: Atom package: Update file-icons to 2.1.34

--- .atom/packages/file-icons/.mocharc.js
+++ .atom/packages/file-icons/.mocharc.js
@@ -1,10 +1,16 @@
 "use strict";
 
-require("mocha-when")({lowercaseIt: false});
-
 module.exports = {
 	bail: !AtomMocha.isCI,
 	require: ["chai/register-should"],
 	slow: 9999,
 	specPattern: /[\\\/]\d+-\w+\.js$/,
+	
+	afterStart(){
+		this.bail && AtomMocha.runner.on("fail", () => {
+			const {remote} = require("electron");
+			if(remote.getCurrentWebContents().isDevToolsOpened())
+				debugger;
+		});
+	},
 };
