9beefb31f47f6ee3647122dcca9a7b99442b3b41
juneboku <juneboku@gmail.com>
2024-10-05 22:08:44 +0900

    .DS_Storeをさっと消すためのfunctionを定義する

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -39,6 +39,29 @@ function gmbd
     git branch --merged $branch | grep -vE "^\*|$branch\$" | xargs -I % git branch -d %
 end
 
+function rmcache
+    if test (count $argv) -eq 0
+        echo "Usage: rmcache <path>"
+        return 1
+    end
+
+    set path $argv[1]
+
+    # Find and list .DS_Store files
+    echo "Listing .DS_Store files in $path:"
+    find $path -name ".DS_Store"
+
+    echo
+
+    # Remove .DS_Store files
+    echo "Removing .DS_Store files..."
+    find $path -name ".DS_Store" -exec rm {} \;
+
+    echo
+
+    echo "Done."
+end
+
 source ~/.config/fish/config_abbr.fish
 starship init fish | source
 status --is-interactive; and source (rbenv init -|psub)
